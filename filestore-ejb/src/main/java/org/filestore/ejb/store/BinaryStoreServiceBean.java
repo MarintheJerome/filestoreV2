bd9899ec21f9df82f8336a1a6192912b5d26b8b7
MarintheJerome <jerome.marinthe@gmail.com>
2016-10-01 11:18:51 +0200

    add persistence

--- filestore-ejb/src/main/java/org/filestore/ejb/store/BinaryStoreServiceBean.java
+++ filestore-ejb/src/main/java/org/filestore/ejb/store/BinaryStoreServiceBean.java
@@ -12,9 +12,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 
 import org.filestore.ejb.config.FileStoreConfig;
 
+@Singleton
+@Startup
 public class BinaryStoreServiceBean implements BinaryStoreService {
 
 	private static final Logger LOGGER = Logger.getLogger(BinaryStoreServiceBean.class.getName());
